575197010577ef0f093799ca7d306f713f4bca3b
Jay Sitter <jsit@users.noreply.github.com>
2021-12-02 09:32:03 -0500

    JJCDV-54: CTA button in CTA banner should be optional (#8)

--- functions.php
+++ functions.php
@@ -887,7 +887,7 @@ acf_add_local_field_group(array(
 							'name' => 'cta',
 							'type' => 'link',
 							'instructions' => '',
-							'required' => 1,
+							'required' => 0,
 							'conditional_logic' => 0,
 							'wrapper' => array(
 								'width' => '',
