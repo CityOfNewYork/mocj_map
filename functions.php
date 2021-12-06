c61879538182621d3e2d576907b9f44411f15786
Jay Sitter <jay.sitter@velir.com>
2021-12-06 17:27:27 -0500

    Domain subdomain data isn't required

--- functions.php
+++ functions.php
@@ -646,7 +646,7 @@ acf_add_local_field_group(array(
 					'name' => 'subdomain_data',
 					'type' => 'repeater',
 					'instructions' => '',
-					'required' => 1,
+					'required' => 0,
 					'conditional_logic' => 0,
 					'wrapper' => array(
 						'width' => '',
