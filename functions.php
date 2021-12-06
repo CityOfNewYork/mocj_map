4e0e4f006eccbdab6c5b95ccb94a3965dbbba76d
Jay Sitter <jay.sitter@velir.com>
2021-12-06 16:27:48 -0500

    Add data indicators ACF options page

--- functions.php
+++ functions.php
@@ -461,6 +461,14 @@ if( function_exists('acf_add_options_page') ) {
         'capability'  => 'edit_posts',
         'redirect'      => false
     ));
+
+    acf_add_options_page(array(
+        'page_title'  => __('Data Indicators', 'mocj'),
+        'menu_title'  => __('Data Indicators', 'mocj'),
+        'menu_slug'   => 'data-indicators',
+        'capability'  => 'edit_posts',
+        'redirect'      => false
+    ));
 }
 
 /**
