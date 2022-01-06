8da155108e1a50b63ca8314302db5d08493f2117
Jay Sitter <jay.sitter@velir.com>
2022-01-06 12:49:05 -0500

    JJCDV-36: Fix whitespace

--- page-domain-overview.php
+++ page-domain-overview.php
@@ -9,55 +9,55 @@ get_header();
 
 <main id="main" class="site">
 
-  <?php
-
-  while ( have_posts() ) : the_post();
-  ?>
-
-  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-    <div class="container type--navy">
-
-      <?php
-	the_title( '<h1 class="type--page-title">', '</h1>' );
-	the_content();
-      ?>
-
-      <div class="domain-grid">
-
 	<?php
-	  if (get_field('domains')) {
-	    foreach (get_field('domains') as $domain) {
-	    ?>
 
-	<div class="domain-grid__domain">
-
-	  <p class="domain-grid__domain-title">
-	    <?php echo $domain['title']; ?>
-	  </p>
-
-	  <div class="domain-grid__domain-description">
-	    <?php echo $domain['description']; ?>
-	  </div>
-
-	  <p class="domain-grid__domain-link">
-	    <a href="<?php print_r($domain['link']); ?>"><?php _e('Learn more', 'mocj'); ?> <?php get_template_part('template-parts/svg/triangle-right'); ?></a>
-	  </p>
-
-	</div>
-
-	    <?php
-	    }
-	  }
+	while ( have_posts() ) : the_post();
 	?>
 
-      </div>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-    </div>
+		<div class="container type--navy">
 
-  </article>
+			<?php
+				the_title( '<h1 class="type--page-title">', '</h1>' );
+				the_content();
+			?>
 
-  <?php endwhile; ?>
+			<div class="domain-grid">
+
+				<?php
+					if (get_field('domains')) {
+						foreach (get_field('domains') as $domain) {
+						?>
+
+				<div class="domain-grid__domain">
+
+					<p class="domain-grid__domain-title">
+						<?php echo $domain['title']; ?>
+					</p>
+
+					<div class="domain-grid__domain-description">
+						<?php echo $domain['description']; ?>
+					</div>
+
+					<p class="domain-grid__domain-link">
+						<a href="<?php print_r($domain['link']); ?>"><?php _e('Learn more', 'mocj'); ?> <?php get_template_part('template-parts/svg/triangle-right'); ?></a>
+					</p>
+
+				</div>
+
+			<?php
+			}
+		}
+	?>
+
+			</div>
+
+		</div>
+
+	</article>
+
+	<?php endwhile; ?>
 
 </main><!-- #main -->
 
--- template-parts/domain-ctabanner.php
+++ template-parts/domain-ctabanner.php
@@ -7,28 +7,28 @@ $cta = $ctabanner['cta'];
 
 <div class="cta-banner bg-dark-blue">
 
-  <div class="container">
+	<div class="container">
 
-  <h2 class="cta-banner__title">
-    <?php echo $ctabanner['title']; ?>
-  </h2>
+	<h2 class="cta-banner__title">
+		<?php echo $ctabanner['title']; ?>
+	</h2>
 
-  <p class="cta-banner__description">
-    <?php echo $ctabanner['description']; ?>
-  </p>
+	<p class="cta-banner__description">
+		<?php echo $ctabanner['description']; ?>
+	</p>
 
 <?php
 if ($cta) {
 ?>
 
-  <a href="<?php echo $cta['url']; ?>" class="domain-cta domain-cta--white" target="<?php echo $cta['target']; ?>">
-    <?php echo $cta['title']; ?>
-  </a>
+	<a href="<?php echo $cta['url']; ?>" class="domain-cta domain-cta--white" target="<?php echo $cta['target']; ?>">
+		<?php echo $cta['title']; ?>
+	</a>
 
 <?php
 }
 ?>
 
-  </div>
+	</div>
 
 </div>
